b1d95d44f2d180ee7b39bbfa6038b9f9f8faf86f
fmitchell <fredric@orbitmedia.com>
2013-08-31 22:02:33 -0500

    inital bootstrap dependencies

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-hello world
+<?php
+
+// Load composer libraries.
+require 'vendor/autoload.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Jotmap on Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <h1>Hello, world!</h1>
+
+    <script src="//code.jquery.com/jquery.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+  </body>
+
+</html>
+
